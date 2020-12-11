7286fa97dded1ee9227ffd20e5a12129d9d44b55
Jürgen Stary <jstary80@gmail.com>
2020-12-11 13:21:24 +0100

    updated lambda

--- model/deployment/app.py
+++ model/deployment/app.py
@@ -8,15 +8,19 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "creds.json"
 
 def lambdaHandler(event, context):
     
-    account_sid = "twilio sid"
-    auth_token  = "twilio token"
+    account_sid = "lsls"
+    auth_token  = "lsals"
     method = event['requestContext']['http']['method']
     path = event['rawPath']    
-    body=json.loads(event['body'])    
+    body=json.loads(event['body'])
+    qa = body['qa']  
     
     """POST / messages endpoint"""
     if method == 'POST' and path == '/messages/':  
        
+        body['input'] = qa[body['input']]
+        
+        #Look up question, and answer and then send to Translation 
         #Infere API - hook your only model or in this case Google Translate
         translate_client = translate.Client()
         if isinstance(body['src'], six.binary_type):
@@ -31,14 +35,15 @@ def lambdaHandler(event, context):
                                             model="nmt")
         
         """Twilio Response"""
-        '''
+        
         client = Client(account_sid, auth_token)
-        message = client.messages.create(
+        '''message = client.messages.create(
             to=body['to'], 
             from_="+18047350925",
-            body=body['input'])
-            print(message.sid)
-        '''        
+            body=result["translatedText"]
+            )
+        print(message.sid)'''
+                
         print(result["translatedText"])
         return {'body': result["translatedText"], 'statusCode': 200}
     
